992049716580afeb7a63fc9623474575c6293ddb
Murad491940 <84373217+Murad491940@users.noreply.github.com>
2023-06-16 21:12:31 +0400

    DateTimeTryParseExact

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ConsoleApp19
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "16-05-23";
+            string str2 = "dd-MM-yy";
+            DateTime a;
+            if(DateTime.TryParseExact(str,str2,CultureInfo.InvariantCulture,DateTimeStyles.None,out a))
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine("wrog number...");
+            }
+        }
+    }
+}
